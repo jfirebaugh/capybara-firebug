9a7d9e96c7d78b5e3abf7bd931f97e0e85b48b3b
John Firebaugh <john.firebaugh@gmail.com>
2011-03-07 08:29:52 -0800

    New feature: enable Firebug on an existing Selenium::WebDriver::Firefox::Profile.

--- lib/capybara/firebug.rb
+++ lib/capybara/firebug.rb
@@ -1,23 +1,28 @@
 require 'selenium/webdriver'
 
+class Selenium::WebDriver::Firefox::Profile
+  def enable_firebug
+    add_extension(File.expand_path("../firebug-1.6.2.xpi", __FILE__))
+
+    # Prevent "Welcome!" tab
+    self["extensions.firebug.currentVersion"] = "999"
+
+    # Enable for all sites.
+    self["extensions.firebug.allPagesActivation"] = "on"
+
+    # Enable all features.
+    ['console', 'net', 'script'].each do |feature|
+      self["extensions.firebug.#{feature}.enableSites"] = true
+    end
+
+    # Closed by default.
+    self["extensions.firebug.previousPlacement"] = 3
+  end
+end
+
 Capybara.register_driver :selenium_with_firebug do |app|
   profile = Selenium::WebDriver::Firefox::Profile.new
-  profile.add_extension(File.expand_path("../firebug-1.6.2.xpi", __FILE__))
-
-  # Prevent "Welcome!" tab
-  profile["extensions.firebug.currentVersion"] = "999"
-
-  # Enable for all sites.
-  profile["extensions.firebug.allPagesActivation"] = "on"
-
-  # Enable all features.
-  ['console', 'net', 'script'].each do |feature|
-    profile["extensions.firebug.#{feature}.enableSites"] = true
-  end
-
-  # Closed by default.
-  profile["extensions.firebug.previousPlacement"] = 3
-
+  profile.enable_firebug
   Capybara::Driver::Selenium.new(app, :browser => :firefox, :profile => profile)
 end
 
