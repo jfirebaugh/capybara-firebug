e87381b13b408f5c808c10700b829ef3b2cf6cbe
Les Hill <leshill@gmail.com>
2011-04-22 14:15:11 -0700

    Refactor spec

--- spec/enable_firebug_spec.rb
+++ spec/enable_firebug_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-describe Selenium::WebDriver::Firefox::Profile, "#enable_firebug" do
-  it "adds the Firebug extension" do
-    subject.should_receive(:add_extension).with(/firebug-1\.6\.2\.xpi$/)
-    subject.enable_firebug
-  end
+describe Selenium::WebDriver::Firefox::Profile do
+  describe "#enable_firebug" do
+    it "adds the Firebug extension" do
+      subject.should_receive(:add_extension).with(/firebug-1\.6\.2\.xpi$/)
+      subject.enable_firebug
+    end
 
-  it "accepts an optional version argument" do
-    subject.should_receive(:add_extension).with(/firebug-1\.7\.0\.xpi$/)
-    subject.enable_firebug("1.7.0")
+    it "accepts an optional version argument" do
+      subject.should_receive(:add_extension).with(/firebug-1\.7\.0\.xpi$/)
+      subject.enable_firebug("1.7.0")
+    end
   end
 end
