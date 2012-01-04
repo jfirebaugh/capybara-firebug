6bf298d8e310f89526a5ab0c0fde6184c1140f01
John Firebaugh <john.firebaugh@gmail.com>
2012-01-03 21:12:32 -0800

    Correct comment

--- lib/capybara-firebug.rb
+++ lib/capybara-firebug.rb
@@ -2,5 +2,5 @@
 # your Gemfile without actually loading any code (which requires the cucumber
 # environment established by the 'cucumber' command).
 
-# To load the firebug, require 'capybara-firebug/cucumber' within a file in
+# To load the firebug, require 'capybara/firebug' within a file in
 # features/support/ or features/step_definitions/.
